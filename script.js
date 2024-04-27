da753679e87a574b5e2f712a36714c540aabe5da
Lucas Henrique <164108216+luskahdmds@users.noreply.github.com>
2024-04-26 21:43:51 -0300

    primeiro comit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Botão 2 - Mudar Tamanho da fonte
+function mudatamanhofonte() {
+    document.getElementById("manipula").style.fontSize = "40pt"
+  }
+  //fim do Botão 2
+  // Botão X - xx
+  function mudacorfonte() {
+    document.getElementById("manipula").style.color = "#ffffff"
+}
+//fim do Botão X
+  // Botão X - xx
+  function backdiv() {
+    document.getElementById("manipula").style.background = "black"
+}
+//fim do Botão X
+  // Botão X - xx
+  function italico() {
+    document.getElementById("manipula").style.fontStyle = "italic"
+}
+//fim do Botão X
+  // Botão X - xx
+  function negrito() {
+    document.getElementById("manipula").style.fontWeight = "bold"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function borda() {
+    document.getElementById("manipula").style.border = "2px solid black"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function excluirborda() {
+    document.getElementById("manipula").style.border = "0px"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function esconderdiv() {
+    document.getElementById("manipula").style.display = "none"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function aparecerdiv() {
+    document.getElementById("manipula").style.display = "Block"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function nao() {
+    document.getElementsByClassName("chamada")[0].innerHTML = "neste botao nao"
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function backgroundp() {
+    document.getElementsByClassName("paragrafo")[0].style.background = 'red'
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function x() {
+
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function x() {
+
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function x() {
+
+  }
+  //fim do Botão X
+  // Botão X - xx
+  function x() {
+
+  }
+//fim do Botão X
